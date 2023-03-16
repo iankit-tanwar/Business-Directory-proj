d9010ac874a968f13e1cad2c492ee850ec0f21ca
iankit-tanwar <tankitofficial01.gmail.com>
2023-03-16 23:23:53 +0530

    register user payload

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,23 +1,46 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import logo from '../../logo.svg';
 
 export default function Navigation() {
-  return (
-    <><Nav
-    activeKey="/home"
-    onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
->
-    <Nav.Item>
-        <Link to="/" className='btn btn-link'>Home</Link>
-    </Nav.Item>
-    <Nav.Item>
-        <Link to='/login' className='btn btn-link'>Login</Link>
-    </Nav.Item>
-    <Nav.Item>
-        <Link to="/register" className='btn btn-link'>Register</Link>
-    </Nav.Item>
+    return (
+        <><Navbar bg="light" expand="lg" className='h-100'>
+            <Container fluid>
+                <Navbar.Brand href="#">
+                    <img
+                        src={logo}
+                        width="100"
 
-</Nav></>
-  )
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Link to="/" className='btn btn-link'>Home</Link>
+                        <Link to="/login" className='btn btn-link'>Login</Link>
+                        <Link to="/register" className='btn btn-link'>Register</Link>
+
+                        
+                    </Nav>
+                    
+                </Navbar.Collapse>
+            </Container>
+        </Navbar></>
+    )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 export default function Register() {
+  //hooks area
+  const [payload, setPayload] = useState({
+    "username": "ankit",
+    "email": "ankit@strapi.io",
+    "password": "Test1234"
+
+
+  });
+
+  let registerUser = () => {
+    alert('okokokok')
+
+
+    let u = document.querySelector('input[name=username]').value;
+    let e = document.querySelector('input[name=email]').value;
+    let p = document.querySelector('input[name=password]').value;
+
+
+    console.log(u)
+    console.log(e)
+    console.log(p)
+
+   
+    setPayload(
+      {
+        "username": u,
+        "email": e,
+        "password": p
+    
+    
+      }
+
+     
+    );
+    console.log(payload)
+
+
+    fetch(`http://localhost:1337/api/auth/local/register`,{
+      method:'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify(payload)
+    })
+    .then((res)=>{res.json()})
+    .then((data)=>{
+      console.log(data)
+    })
+    .catch(err=>err);
+      
+
+
+
+  }
+
+
+
   return (
     <>
-    <h1>HOME REGISTER</h1>
+      <h1>HOME REGISTER</h1>
+      <Form>
+        <Form.Group className="mb-3" >
+          <Form.Label>User Name</Form.Label>
+          <Form.Control name="username" id='username' type="text" placeholder="User Name" />
+          <Form.Text className="text-muted">
+
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Email address</Form.Label>
+          <Form.Control name="email" id='email' type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control name="password" id='password' type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="button" onClick={() => { registerUser() }}>
+          Submit
+        </Button>
+      </Form>
     </>
   )
 }
