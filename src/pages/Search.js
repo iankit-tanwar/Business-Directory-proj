a731718b594f496b308d81ada1ab7f3fac2625a4
iankit-tanwar <tankitofficial01.gmail.com>
2023-03-25 12:13:39 +0530

    search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,53 +9,63 @@ import { URL } from '../helper/url';
 
 export default function Search() {
 
-    const[searchParams , setSearchParams]= useSearchParams();
-useEffect(()=>{
- 
-    console.log('cat_name--->', searchParams.get('cat_name'));
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [businesses, setBusinesses] = useState([]);
 
-    fetch(`${URL}/api/businesses?populate=*&filters[business_categories][name][$containsi]=${searchParams.get('cat_name')}`,{})
-    .then(res=>res.json())
-    .then((data)=>{console.log(data)})
-    .catch(err=>err);
-},[]);
+    useEffect(() => {
+
+        console.log('cat_name--->', searchParams.get('cat_name'));
+
+        fetch(`${URL}/api/businesses?populate=*&filters[business_categories][name][$containsi]=${searchParams.get('cat_name')}`, {})
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data.data)
+                setBusinesses(data.data);
+            })
+            .catch(err => err);
+    }, []);
 
     return (
         <>
             <Row>
                 <Col sm={9}>  <h1>Search Detail</h1>
+                    {
+                        businesses.map((cv, idx, arr) => {
+                           
+                         return   <Card key={idx} className='p-3 mb-3'>
 
-                    <Card className='p-3'>
-
-                        <Row>
-                            <Col sm={3} >  <Card.Img className="img-fluid" variant="top" src="https://content.jdmagicbox.com/comp/mumbai/g9/022pxx22.xx22.130218195944.z7g9/catalogue/hotel-mina-international-jogeshwari-west-mumbai-hotels-sksukak303-250.jpg?w=3840&q=75" /></Col>
-                            <Col sm={9}><Card.Body>
-                                <Card.Title> Hotel Mina International</Card.Title>
-
-                               
-                                
-                                <Badge  className='p-2 fs-6 me-2' bg="success">
-                                    4.0
-                                </Badge>  <span ><FontAwesomeIcon className=' text-warning' icon={faStar}/>
-                                <FontAwesomeIcon className='text-warning' icon={faStar}/>
-                                <FontAwesomeIcon className='text-warning' icon={faStar}/>
-                                <FontAwesomeIcon className='text-warning' icon={faStar}/>
-                                <FontAwesomeIcon className='text-secondary' icon={faStar}/></span>
-                                <span className='ms-2'>4,777 Rating</span> 
-
-                                
-
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button className='btn btn-succes'>show number</Button>
-                            </Card.Body></Col>
+                                <Row>
+                                    <Col sm={3} >  <Card.Img className="img-fluid" variant="top" src={ URL +cv.attributes.photo.data[0].attributes.url} /></Col>
+                                    <Col sm={9}><Card.Body>
+                                        <Card.Title> {cv.attributes.name}</Card.Title>
 
 
 
-                        </Row>
-                    </Card>
+                                        <Badge className='p-2 fs-6 me-2' bg="success">
+                                            4.0
+                                        </Badge>  <span ><FontAwesomeIcon className=' text-warning' icon={faStar} />
+                                            <FontAwesomeIcon className='text-warning' icon={faStar} />
+                                            <FontAwesomeIcon className='text-warning' icon={faStar} />
+                                            <FontAwesomeIcon className='text-warning' icon={faStar} />
+                                            <FontAwesomeIcon className='text-secondary' icon={faStar} /></span>
+                                        <span className='ms-2'>4,777 Rating</span>
+
+
+
+                                        <Card.Text>
+                                           {cv.attributes.desc}
+                                        </Card.Text>
+                                        <Button className='btn btn-succes'>show number</Button>
+                                    </Card.Body></Col>
+
+
+
+                                </Row>
+                            </Card>
+                        })
+                    }
+
+
                 </Col>
                 <Col sm={3}>
                     <Card style={{ width: '18rem' }}>
